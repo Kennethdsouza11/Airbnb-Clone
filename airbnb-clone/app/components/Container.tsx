172431fc9e60173ff0f4d3879cbd5222d6836af2
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-10-17 00:49:29 +0530

    created container component and added interface

---
+++ airbnb-clone/app/components/Container.tsx
@@ -0,0 +1,13 @@
+import { ReactChildren } from "react";
+
+interface ContainerProps{
+    children : React.ReactNode;
+}
+
+const Container : React.FC<ContainerProps> = ({children}) => {
+    return (
+        <div>{children}!</div>
+    );
+}
+
+export default Container;
