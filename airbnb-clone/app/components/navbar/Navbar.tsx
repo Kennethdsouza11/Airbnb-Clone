06c2058721c5f06e81eddf5d7033c9625ca3947f
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-10-17 00:39:36 +0530

    added style and made some changes

--- airbnb-clone/app/components/navbar/Navbar.tsx
+++ airbnb-clone/app/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
-        <div>
-            I am a navbar
+        <div className = 'fixed w-full bg-white z-10 shadow-sm'>
+            <div className = 'py-4 border-b-[1px]'></div>
         </div>
     );
 }
