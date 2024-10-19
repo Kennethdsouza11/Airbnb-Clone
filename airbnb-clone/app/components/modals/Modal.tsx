e9d82f5e433c8187f751f7ed86a945cc09a36763
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-10-20 00:08:44 +0530

    added close and open function as well as a closing delay

--- airbnb-clone/app/components/modals/Modal.tsx
+++ airbnb-clone/app/components/modals/Modal.tsx
@@ -1,8 +1,56 @@
 'use client';
 
-const Modal = () => {
+import React from "react";
+import { useEffect, useState } from 'react';
+
+// useCallback memorizes the function so that it only gets re-created if any dependencies changes.
+
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+    secondaryAction?: () => void;
+    secondaryLabel?: string;
+}
+const Modal: React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled,
+    secondaryAction,
+    secondaryLabel
+
+}) => {
+    const [showModal, setShowModal] = useState(isOpen);
+
+    useEffect(() => {
+        setShowModal(isOpen);
+
+    }, [isOpen]);
+
+    const handleClose = useCallback(() => {
+        if (disabled) {
+            return;
+        }
+
+        setShowModal(false);
+
+        //creating a closing animation which delays the onClose function by 300 ms
+        setTimeout(()=> {
+            onClose();
+        }, 300)
+    }, [disabled, onClose]);
     return (
-        <div></div>
+        <div> </div>
     );
 }
 
