4654d2d4a8cd06c3291741ca04177c147d1eac36
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-10-20 00:10:06 +0530

    formatted the doc

--- airbnb-clone/app/components/modals/Modal.tsx
+++ airbnb-clone/app/components/modals/Modal.tsx
@@ -1,57 +1,53 @@
-'use client';
+"use client";
 
 import React from "react";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 // useCallback memorizes the function so that it only gets re-created if any dependencies changes.
 
 interface ModalProps {
-    isOpen?: boolean;
-    onClose: () => void;
-    onSubmit: () => void;
-    title?: string;
-    body?: React.ReactElement;
-    footer?: React.ReactElement;
-    actionLabel: string;
-    disabled?: boolean;
-    secondaryAction?: () => void;
-    secondaryLabel?: string;
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryLabel?: string;
 }
 const Modal: React.FC<ModalProps> = ({
-    isOpen,
-    onClose,
-    onSubmit,
-    title,
-    body,
-    footer,
-    actionLabel,
-    disabled,
-    secondaryAction,
-    secondaryLabel
-
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryLabel,
 }) => {
-    const [showModal, setShowModal] = useState(isOpen);
+  const [showModal, setShowModal] = useState(isOpen);
 
-    useEffect(() => {
-        setShowModal(isOpen);
+  useEffect(() => {
+    setShowModal(isOpen);
+  }, [isOpen]);
 
-    }, [isOpen]);
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
 
-    const handleClose = useCallback(() => {
-        if (disabled) {
-            return;
-        }
+    setShowModal(false);
 
-        setShowModal(false);
-
-        //creating a closing animation which delays the onClose function by 300 ms
-        setTimeout(()=> {
-            onClose();
-        }, 300)
-    }, [disabled, onClose]);
-    return (
-        <div> </div>
-    );
-}
+    //creating a closing animation which delays the onClose function by 300 ms
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [disabled, onClose]);
+  return <div> </div>;
+};
 
 export default Modal;
