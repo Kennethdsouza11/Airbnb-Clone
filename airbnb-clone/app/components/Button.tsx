ee55167a3e5b94e6baf60038c5ec154ed8cfe8bc
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-10-20 18:52:13 +0530

    added interface

--- airbnb-clone/app/components/Button.tsx
+++ airbnb-clone/app/components/Button.tsx
@@ -1,8 +1,19 @@
 'use client';
 
+import { IconType } from "react-icons";
+
+interface ButtonProp {
+    label: string;
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    disabled?: boolean;
+    outline?: boolean;
+    small?: boolean;
+    icon?: IconType
+}
+
 const Button = () => {
     return (
-        <button></button>
+        <button> </button>
     );
 }
 
