694084aa4dedbb80d60a061b90409ce4e795fd48
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-12-09 16:21:24 +0530

    added config to add images into exernal websites

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    //remotePatterns is useful when we want to specify patterns for external image sources. When the app needs to display images that are hosted on external servers.
+    remotePatterns: [
+      {
+        hostname: "a0.muscache.com",//domain name of the external server that is allowed to serve images.
+        protocol: "https",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
