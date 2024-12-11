f2ad73718f169ea03c0a1a45be91c01bfcd6b8b0
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-12-11 12:56:56 +0530

    added a section to choose category from a grid

--- app/components/SelectCategory.tsx
+++ app/components/SelectCategory.tsx
@@ -6,9 +6,17 @@ import Image from "next/image";
 import { useState } from "react";
 
 export function SelectCategory() {
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null); //this is array destructuring. We extract values from arrays. <string | null> means it can either hold string or null value and it
+  const [selectedCategory, setSelectedCategory] = useState<string | undefined>(
+    undefined
+  ); //this is array destructuring. We extract values from arrays. <string | null> means it can either hold string or null value and it
   return (
     <div className="grid grid-cols-4 gap-8 mt-10 w-3/5 mx-auto mb-36">
+      <input
+        type="hidden"
+        name="categoryName"
+        value={selectedCategory as string}
+      />
+      {/* This is hidden from the user and is not shown in the webpage. It is likely used in a form submission process to send the selected category's value to a server or backend system.*/}
       {categoryItems.map((item) => (
         <div key={item.id} className="cursor-pointer">
           <Card
