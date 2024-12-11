c2ceeafe81ed5085a7f54f57186f3835c163057d
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-12-11 13:04:31 +0530

    fixed a major bug that doesnt let me go to the structure page

---
+++ app/actions.ts
@@ -0,0 +1,89 @@
+"use server"; //runs on the server side only like database interactions
+//actions are also called server actions.
+import prisma from "./lib/db";
+import { redirect } from "next/navigation";
+import { supabase } from "./lib/supabase";
+import path from "path";
+
+//if the user does not a have home under his name then we redirect him to the page to create a home.
+export async function createAirbnbHome({ userId }: { userId: string }) {
+  const data = await prisma.home.findFirst({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      createdAt: "desc", //descending order.
+    },
+  });
+
+  if (data == null) {
+    const data = await prisma.home.create({
+      data: {
+        userId: userId,
+      },
+    });
+    return redirect(`/create/${data.id}/structure`);
+  } else if (
+    !data.addedCategory &&
+    !data.addedDescription &&
+    !data.addedLocation
+  ) {
+    return redirect(`/create/${data.id}/structure`);
+  } else if (data.addedCategory && !data.addedDescription) {
+    return redirect(`/create/${data.id}/description`);
+  }
+}
+
+export async function createCategoryPage(formData: FormData) {
+  const categoryName = formData.get("categoryName") as string;
+  const homeId = formData.get("homeId") as string;
+  const data = await prisma.home.update({
+    where: {
+      id: homeId,
+    },
+    data: {
+      categoryName: categoryName,
+      addedCategory: true,
+    },
+  });
+
+  return redirect(`/create/${homeId}/description`);
+}
+
+export async function CreateDescription(formData: FormData) {
+  const title = formData.get("title") as string;
+  const description = formData.get("description") as string;
+  const price = formData.get("price");
+  const imageFile = formData.get("image") as File;
+  const homeId = formData.get("homeId") as string;
+  const guestNumber = formData.get("guests") as string;
+  const roomNumber = formData.get("room") as string;
+  const bathroomNumber = formData.get("bathroom") as string;
+
+  //to upload the images we follow a different procedure
+  const { data: imageData } = await supabase.storage
+    .from("images")
+    .upload(`${imageFile.name}-${new Date()}`, imageFile, {
+      cacheControl: "2592000", //30days in seconds
+      contentType: "image/png",
+    });
+  // supabase.storage.from("images") connects to the images folder in supabase's cloud storage. upload here uploads a file to that storage. It takes three main things: file name (${imageFile.name} - ${new Date()} creates a unique name by combining the file's original name with the current date and time) the file itself which is imageFile and Settings which is an object that specifies additional options for the upload : cacheControl, this sets for how long the file can be cached by browsers and contentType specifies the type of the file here it is png. After the upload is done, the result is stored in the data variable which is used for further processing or confirmation. Cache control is a way to tell browsers or websites how long they can save a file like an img or a video, so that they dont have to download it every time someone visits a page.
+
+  const data = await prisma.home.update({
+    where: {
+      id: homeId,
+    },
+    data: {
+      title: title,
+      description: description,
+      price: Number(price),
+      bedrooms: roomNumber,
+      bathrooms: bathroomNumber,
+      guests: guestNumber,
+      photo: imageData?.path,
+      addedDescription: true,
+    },
+  });
+
+  return redirect(`/create/${homeId}/address`); //redirect to the address page
+}
