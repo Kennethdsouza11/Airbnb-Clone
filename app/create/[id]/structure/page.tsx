4a1d4929ef4e8cf15304741fc80a98babb27351a
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-12-11 13:21:45 +0530

    page for structure route

--- app/create/[id]/structure/page.tsx
+++ app/create/[id]/structure/page.tsx
@@ -1,26 +1,26 @@
+import { createCategoryPage } from "@/app/actions";
+import { CreationBottomBar } from "@/app/components/CreationBottomBar";
 import { SelectCategory } from "@/app/components/SelectCategory";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-export default function StructureRoute() {
+
+export default function StructureRoute({
+  params
+}: {
+  params: { id: string };
+}) {
   return (
+    //here params is passed as an argument to the StructureRoute and params must have a property called id which is of type string.
     <>
       <div className="w-3/5 mx-auto">
         <h2 className="text-3xl font-semibold tracking-tight transition-colors">
           Which of these best describes your Home?
         </h2>
       </div>
-      <form>
+      <form action={createCategoryPage}>
+        <input type="hidden" name="homeId" value={params.id} />
         <SelectCategory />
-        <div className="fixed w-full bottom-0 z-10 bg-white  border-t h-24">
-          <div className="flex items-center justify-between mx-auto px-5 lg:px-10 h-full">
-            <Button variant="secondary" size="lg" asChild>
-              <Link href="/">Cancel</Link>
-            </Button>
-
-            <Button size="lg">Save</Button>
-          </div>
-        </div>
+        <CreationBottomBar />
       </form>
     </>
   );
 }
+//we have used <form> element as its intent to collect and submit user input whereas <div> element does not do so.
