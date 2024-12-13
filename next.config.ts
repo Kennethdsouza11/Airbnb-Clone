f15b708cbaefe31c6e95e8e37b39948ac7a5c8ad
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-12-13 11:02:24 +0530

    added domain name of the image url for the dashboard

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,12 @@ const nextConfig: NextConfig = {
     //remotePatterns is useful when we want to specify patterns for external image sources. When the app needs to display images that are hosted on external servers.
     remotePatterns: [
       {
-        hostname: "a0.muscache.com",//domain name of the external server that is allowed to serve images.
+        hostname: "a0.muscache.com", //domain name of the external server that is allowed to serve images.
+        protocol: "https",
+        port: "",
+      },
+      {
+        hostname: "vbywagjdfoiwossjltkg.supabase.co", //domain name of the image url for the dashboard
         protocol: "https",
         port: "",
       },
