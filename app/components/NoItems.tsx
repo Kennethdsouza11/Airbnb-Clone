3c0d8f815ed68c8a86774b8065ce4065add358eb
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-12-13 22:53:25 +0530

    to display message when no info is available

--- app/components/NoItems.tsx
+++ app/components/NoItems.tsx
@@ -1,16 +1,19 @@
-import { File } from "lucide-react";
+import { File, FileQuestion } from "lucide-react";
 
-export function NoItems() {
+interface IappProps {
+  title: string;
+  description: string;
+}
+
+export function NoItems({ description, title }: IappProps) {
   return (
     <div className="flex min-h-[600px] flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50 mt-10">
       <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10">
-        <File className="h-10 w-10 text-primary" />
+        <FileQuestion className="h-10 w-10 text-primary" />
       </div>
-      <h2 className="mt-6 text-xl font-semibold">
-        Uh Oh! No listings for this categories
-      </h2>
+      <h2 className="mt-6 text-xl font-semibold">{title}</h2>
       <p className="text-center text-sm leading-6 text-muted-foreground mt-2">
-        Please check other category...
+        {description}
       </p>
     </div>
   );
