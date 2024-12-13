a306343a64992ed6d6afb78b6128fdadb7690828
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-12-13 10:59:06 +0530

    added various functionality like loading stage error handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,89 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import prisma from "./lib/db";
 import { MapFilterItems } from "./components/MapFilterItems";
-export default function Home() {
+import { ListingCard } from "./components/ListingCard";
+import { Suspense } from "react";
+import { SkeletonCard } from "./components/SkeletonCard";
+import { NoItems } from "./components/NoItems";
+
+async function getData({
+  searchParams,
+}: {
+  searchParams?: { filter?: string };
+}) {
+  const data = await prisma.home.findMany({
+    where: {
+      addedCategory: true,
+      addedLocation: true,
+      addedDescription: true,
+      categoryName: searchParams?.filter ?? undefined,
+    },
+    select: {
+      photo: true,
+      id: true,
+      price: true,
+      description: true,
+      country: true,
+    },
+  });
+  return data;
+}
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: { filter?: string };
+}) {
+  //searchParams is the string after the ? in the URl for example if /home?filter=popular is the url then searchParams will be filter: 'popular'
+
   return (
-    <div className = "container mx-auto px-5 lg:px-10">
-      <MapFilterItems/>
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
+      <Suspense key={searchParams?.filter} fallback={<SkeletonLoading />}>
+        <ShowItems searchParams={searchParams} />
+      </Suspense>
+    </div>
+  );
+}
+
+async function ShowItems({
+  searchParams,
+}: {
+  searchParams?: { filter?: string };
+}) {
+  const data = await getData({ searchParams: searchParams });
+  return (
+    <>
+      {data.length === 0 ? (<NoItems />) : (
+      <div className="grid lg-grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+        {data.map((item) => (
+          <ListingCard
+            key={item.id}
+            imagePath={item.photo as string}
+            description={item.description as string}
+            location={item.country as string}
+            price={item.price as number}
+          /> //key is used to tell the items apart
+        ))}
+      </div>
+      )}
+    </>
+  );
+}
+
+function SkeletonLoading() {
+  return (
+    <div className="grid lg-grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
     </div>
   );
 }
